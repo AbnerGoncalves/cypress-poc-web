6544a12cfd2cf597cbad41e89d9ddd4d95bb6400
Abner <abnergoncalves3@gmail.com>
2023-01-20 15:00:05 -0300

    primeiro code

---
+++ cypress/utils/constants/consts.js
@@ -0,0 +1,17 @@
+class ConstantsObjects{
+
+    input_name = "input[name='name']";
+    input_cpf = "input[name='cpf']";
+    input_email = "input[name='email']";
+    input_whats = "input[name='whatsapp']";
+
+    input_cep = "input[name='postalcode']"
+    input_num_resid = "input[name='address-number']"
+
+    option_moto = '.delivery-method > :nth-child(1)'
+    option_bicicleta = '.delivery-method > :nth-child(2)'
+    option_van = '.delivery-method > :nth-child(3)'
+    
+
+}
+export default ConstantsObjects
